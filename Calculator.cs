8fc6cbbcf2722ebcd1e5deeaf44da4b699947bed
fakhriikhwll <fakhrian.im@gmail.com>
2019-03-19 14:42:08 +0700

    MethodStatic

---
+++ Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Calculator {
+
+
+        public int penjumlahan(int a, int b) {
+            return a + b;
+        }
+
+        // overload method
+        public int penjumlahan(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        public int pengurangan(int a, int b) {
+            return a - b;
+        }
+
+        // method static
+        public static int perkalian(int a, int b) {
+            return a * b;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int hasil;
+
+            Calculator calculator = new Calculator();
+
+            hasil = calculator.penjumlahan(10, 2);
+            CetakHasil("Hasil Penjumlahan : " + hasil);
+
+            hasil = calculator.penjumlahan(10, 2, 3);
+            CetakHasil("Hasil Penjumlahan overload: " + hasil);
+
+            hasil = calculator.pengurangan(7, 2);
+            CetakHasil("Hasil pengurangan: " + hasil);
+
+            hasil = Calculator.perkalian(5, 2);
+            CetakHasil("Hasil Perkalian: " + hasil);
+
+            Console.ReadKey();
+        }
+
+        static void CetakHasil(string hasil) {
+            Console.WriteLine(hasil);
+        }
+    }
+}
